c7672f1043f349498356133b48303c4d40b4d485
Michael Rahn <michael.rahn@digistore24.team>
2021-01-25 22:23:04 +0100

    Create reproducible app state.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from "@angular/core";
+import { IonRefresher } from "@ionic/angular";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
 export class HomePage {
+  @ViewChild("refresher") refresher: IonRefresher;
+  isLoading = false;
 
   constructor() {}
 
+  async fetch() {
+    try {
+      this.isLoading = true;
+      await this.fakeFetchData();
+    } finally {
+      this.isLoading = false;
+      this.refresher?.complete();
+    }
+  }
+
+  async handlePullToRefresh() {
+    await this.fetch();
+  }
+
+  async fakeFetchData() {
+    return new Promise((resolve) => {
+      setTimeout(() => resolve(true), 2000);
+    });
+  }
 }
